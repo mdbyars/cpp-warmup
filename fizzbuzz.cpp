c591aceb7a7f03f36c246c4a24a5a4e0e559425b
Monique Byars <evettesniche@gmail.com>
2021-03-04 16:45:23 -0600

    added fizz buzz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -1,16 +1,36 @@
-// Name:
-// Date:
-
+//Monique Byars;
+//02 / 24 / 2021;
 #include <iostream>
+#include <list>
+#include <sstream>
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
-   int X, Y, N;
-   cin >> X;
-   cin >> Y;
-   cin >> N;
+	int X, Y, N;
+	cin >> X;
+	cin >> Y;
+	cin >> N;
+	for (int i = 1; i < N + 1; i++) {
+		if (i % X == 0) {
+			if (i % Y == 0) {
+				//cout << i;
+				cout << "FizzBuzz \n";
+			}
+			else {
+				//cout << i;
+				cout << "Fizz \n";
+			}
+		}
+		else if (i % Y == 0) {
+			//	cout << i;
+			cout << "Buzz \n";
+		}
+		else {
+			cout << i;
+			cout << "\n";
+		}
+	}
 
-   // Your code here
 }
-
